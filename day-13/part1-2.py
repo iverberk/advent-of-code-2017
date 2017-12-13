60db74996a51e345fa29e3c04c273a60e2c22126
Ivo Verberk <iverberk@xebia.com>
2017-12-13 15:52:08 +0100

    Day 13 solutions.

---
+++ day-13/part1-2.py
@@ -0,0 +1,33 @@
+layers = {}
+
+with open("firewall.in") as f:
+    for layer in f:
+        d, r = [int(l) for l in layer.split(': ')]
+        layers[d] = {
+            'range': r,
+            'interval': r*2-2
+        }
+
+def run(delay = 0, stop_when_caught = False):
+    severity = 0
+    depth = 0
+    ps = delay
+    caught = False
+    while depth <= max(layers.keys()):
+        if depth in layers and ps % (layers[depth]['interval']) == 0:
+            caught = True
+            if stop_when_caught:
+                return caught, severity
+            severity += depth * layers[depth]['range']
+        depth += 1
+        ps += 1
+
+    return caught, severity
+
+print(run(0, False))
+
+delay = 1
+while run(delay, True)[0]:
+    delay += 1
+
+print(delay)
