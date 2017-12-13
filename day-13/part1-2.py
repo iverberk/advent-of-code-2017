8c767664dd96df7b7440d2bf2b045f5a190cedcb
Ivo Verberk <iverberk@xebia.com>
2017-12-13 16:17:03 +0100

    Get rid of incredibly inefficient max calculation in loop.

--- day-13/part1-2.py
+++ day-13/part1-2.py
@@ -1,4 +1,5 @@
 layers = {}
+max_depth = 0
 
 with open("firewall.in") as f:
     for layer in f:
@@ -7,13 +8,14 @@ with open("firewall.in") as f:
             'range': r,
             'interval': r*2-2
         }
+        max_depth = max(max_depth, d)
 
 def run(delay = 0, stop_when_caught = False):
     severity = 0
     depth = 0
     ps = delay
     caught = False
-    while depth <= max(layers.keys()):
+    while depth <= max_depth:
         if depth in layers and ps % (layers[depth]['interval']) == 0:
             caught = True
             if stop_when_caught:
