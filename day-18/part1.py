e34e37150f634ade2d4b0847c86ccf336bf05866
Ivo Verberk <iverberk@xebia.com>
2017-12-18 22:28:27 +0100

    Optimize code a little bit.

--- day-18/part1.py
+++ day-18/part1.py
@@ -6,16 +6,11 @@ instructions = []
 with open('instructions.in') as f:
     for instruction in f:
         instruction =  instruction.split()
-        operand = None
-        if len(instruction) == 3:
-            operand = instruction[2]
-
+        operand = instruction[2] if len(instruction) == 3 else None
         instructions.append((instruction[0], instruction[1], operand))
 
 # Get the register value or return the plain number
 def get(operand):
-    if operand == None:
-        return operand
     try:
         return int(operand)
     except ValueError:
@@ -28,24 +23,26 @@ while 0 <= position < len(instructions):
     opcode, operand1, operand2 = instructions[position]
 
     # convert to number or find value in registers
-    operand2 = get(operand2)
+    v1 = get(operand1)
+    if operand2:
+        v2 = get(operand2)
 
     if opcode == 'set':
-        registers[operand1] = operand2
+        registers[operand1] = v2
     elif opcode == 'mul':
-        registers[operand1] = get(operand1) * operand2
+        registers[operand1] = v1 * v2
     elif opcode == 'jgz':
-        if get(operand1) > 0:
-            position += operand2
+        if v1 > 0:
+            position += v2
             continue
     elif opcode == 'add':
-        registers[operand1] = get(operand1) + operand2
+        registers[operand1] = v1 + v2
     elif opcode == 'mod':
-        registers[operand1] = get(operand1) % operand2
+        registers[operand1] = v1 % v2
     elif opcode == 'snd':
-        frequency = get(operand1)
+        frequency = v1
     elif opcode == 'rcv':
-        if get(operand1) != 0:
+        if v1 != 0:
             print(frequency)
             break
     else:
--- day-18/part2.py
+++ day-18/part2.py
@@ -1,28 +1,18 @@
-import logging
 from concurrent.futures.thread import ThreadPoolExecutor
 import queue
 from collections import defaultdict
 
-logging.basicConfig(level=logging.INFO, format='%(threadName)s %(message)s')
-logger = logging.getLogger('duet')
-
 instructions = []
-q0 = queue.Queue()
-q1 = queue.Queue()
+q0, q1 = queue.Queue(), queue.Queue()
 
 with open('instructions.in') as f:
     for instruction in f:
         instruction =  instruction.split()
-        operand = None
-        if len(instruction) == 3:
-            operand = instruction[2]
-
+        operand = instruction[2] if len(instruction) == 3 else None
         instructions.append((instruction[0], instruction[1], operand))
 
 # Get the register value or return the plain number
 def get(operand, registers):
-    if not operand:
-        return None
     try:
         return int(operand)
     except ValueError:
@@ -32,26 +22,27 @@ def run(p, receive_q, send_q):
     registers = defaultdict(int)
     registers['p'] = p
 
-    sends = 0
-    position = 0
+    sends, position = 0, 0
     while 0 <= position < len(instructions):
         opcode, operand1, operand2 = instructions[position]
 
         # convert to number or find value in registers
-        operand2 = get(operand2, registers)
+        v1 = get(operand1, registers)
+        if operand2:
+            v2 = get(operand2, registers)
 
         if opcode == 'set':
-            registers[operand1] = operand2
+            registers[operand1] = v2
         elif opcode == 'mul':
-            registers[operand1] = get(operand1, registers) * operand2
+            registers[operand1] = v1 * v2
         elif opcode == 'jgz':
-            if get(operand1, registers) > 0:
-                position += operand2
+            if v1 > 0:
+                position += v2
                 continue
         elif opcode == 'add':
-            registers[operand1] = get(operand1, registers) + operand2
+            registers[operand1] = v1 + v2
         elif opcode == 'mod':
-            registers[operand1] = get(operand1, registers) % operand2
+            registers[operand1] = v1 % v2
         elif opcode == 'snd':
             send_q.put(get(operand1, registers))
             sends += 1
@@ -59,10 +50,9 @@ def run(p, receive_q, send_q):
             try:
                 registers[operand1] = receive_q.get(True, 0.1)
             except queue.Empty:
-                logger.info("Blocked waiting for data, terminating!")
                 break
         else:
-            logger.log("Invalid instruction found: {}".format(opcode))
+            print("Invalid instruction found: {}".format(opcode))
 
         position += 1
 
